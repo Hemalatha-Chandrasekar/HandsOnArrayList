88041b1bed5c880d1e1d055b6f89ff7080684206
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-11 08:15:57 -0600

    HandsOnArrayList Commit

--- src/HandsOnArrayList.java
+++ src/HandsOnArrayList.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class HandsOnArrayList {
+public void ArrayListDemo(){
+        ArrayList<String> students = new ArrayList<>();
+        students.add("John");
+        students.add("Jane");
+        students.add("Bob");
+        students.add("Mary");
+        students.add("Jack");
+        // Print the list of names
+        System.out.println("The Original list of the Students:"+students);
+        //Remove a name and print the updated list
+        students.remove("John");
+        System.out.println("Removed John from list: "+students);
+        // Sort the list alphabetically and print the sorted list.
+        Collections.sort(students);
+        System.out.println("The Sorted list of Students:"+students);
+    }
 }
